77becff9658e751d5b9265ee70028ac844f2e21f
Ritvik Arora <81797303+RitvikArorarara@users.noreply.github.com>
2025-01-06 22:14:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from "./components/Home"
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div >
+      <NavBar />
+      <Home />
+      <About />
+      <Projects/>
+      <Skills />
+      <Contact/>
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
